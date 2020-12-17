22cdd1353fa776a30c883307d77ab6337d1be406
dannymckinney88 <dannymckinney88@gmail.com>
2020-12-16 23:44:16 -0700

    Adds button to single product page

--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,43 +1,74 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect, useContext } from "react";
 import { commerce } from "../lib/commerce";
-import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
-import "../styles/single.css"
-
+import ReactHtmlParser from "react-html-parser";
+import "../styles/single.css";
+import { OrderContext } from "../context/Order";
+import { CartContext } from "../context/Cart";
 
 const SingleProduct = (props) => {
+  const [product, setProduct] = useState();
 
-    const [product, setProduct] = useState()
+  const [order] = useContext(OrderContext);
+  const [cart, setCart] = useContext(CartContext);
+  const productId = props.match.params.id;
+  const handleAddToCart = () => {
+    commerce.cart
+      .add(productId, 1)
+      .then((item) => {
+        setCart({ cart: item.cart });
+      })
+      .catch((error) => {
+        console.error("There was an error adding the item to the cart", error);
+      });
+  };
 
-    // console.log(product.description)
-    const fetchProduct = () =>{
-        const productId = props.match.params.id
-        commerce.products.retrieve(productId)
-        .then((product) => {
-            console.log(product)
-            setProduct(product)
-        });
-
-    }
-    console.log(product)
-    useEffect( () =>{fetchProduct()},[])
-    return (
-        <>
-        {product? 
-            <div className="container-fluid">
-            <div className="row product-wrapper">
-                <div className="col">
-                    <img className="product-info-img" src={product.media.source} alt=""/>
-                </div>
-                <div className="col product-info">
-                    <h4>Contents</h4>
-                    {product? ReactHtmlParser(product.description) : ""}
-                </div>
+  const fetchProduct = () => {
+    
+    commerce.products.retrieve(productId).then((product) => {
+      setProduct(product);
+    });
+  };
+  console.log(product);
+  useEffect(() => {
+    fetchProduct();
+  }, []);
+  return (
+    <>
+      {product ? (
+        <div className="container-fluid">
+          <div className="row product-wrapper">
+            <div className="col-4">
+            <h2>hello</h2>
+              <img
+                className="product-info-img"
+                src={product.media.source}
+                alt=""
+              />
+           
             </div>
+            <div className="col ">
+            <div className=" product-info">
+              <h4>Contents</h4>
+              {product ? ReactHtmlParser(product.description) : ""}
+
+            </div>
+            </div>
+            <div className="col-2">
+            <button
+             
+                className="single-btn"
+                onClick={handleAddToCart}
+              >
+                Add to cart
+              </button>
+            </div>
+          </div>
         </div>
-            : ""
-        }
-        </>
-    );
-}
+      ) : (
+        ""
+      )}
+    </>
+  );
+};
 
 export default SingleProduct;
