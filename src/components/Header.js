6fb2582b1261879af6d94f3e37417da6f4e30707
dannymckinney88 <dannymckinney88@gmail.com>
2020-12-16 22:52:51 -0700

    changes A tag to button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,9 @@
-import React, { useState, useEffect } from "react";
-import { Button } from "react-bootstrap";
+import React, { useState } from "react";
 import "../styles/nav.css";
 import { Link } from "react-router-dom";
 import Cart from "../pages/Cart";
-import logo from "../img/logo.png";
 
-const Header = (props) => {
-  // console.log(props.cart)
+const Header = () => {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
@@ -37,35 +34,12 @@ const Header = (props) => {
         <div className="nav-right">
         <ul >
             <li>
-                <a variant="primary" onClick={handleShow}> <i class="fas fa-shopping-cart fa-2x"></i></a>
+                <button href="#"  className="cart-nav-btn" onClick={handleShow}> <i class="fas fa-shopping-cart fa-2x"></i></button>
                 <Cart show={show} handleShow={handleShow} handleClose={handleClose}/>
             </li>
         </ul>
     </div>
       </div>
-      {/* <nav className="navbar  navbar-dark bg-dark">
-    <div >
-        <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-            <Link to={'/'}> Home </Link>
-            </li>
-            <li className="nav-item">
-                <Link to={'/allproducts'}>   All Product </Link>
-            </li>
-        </ul>
-    </div>
-    <div >
-        <h6 className="navbar-brand mx-auto" href="#">Danny's sweet shack</h6>
-    </div>
-    <div className="navbar">
-        <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-                <a variant="primary" onClick={handleShow}> <i class="fas fa-shopping-cart fa-2x"></i></a>
-                <Cart show={show} handleShow={handleShow} handleClose={handleClose}/>
-            </li>
-        </ul>
-    </div>
-</nav> */}
     </nav>
   );
 };
