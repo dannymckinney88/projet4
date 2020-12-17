ca8f56caa3d25025670cb739fa3dafae061f446c
dannymckinney88 <dannymckinney88@gmail.com>
2020-12-16 18:41:23 -0700

    Changes nav bar and adds cart logo from font awesome

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,47 +1,71 @@
-import React, {useState, useEffect} from "react";
-import {Button} from 'react-bootstrap'
-import '../styles/nav.css'
-import {Link} from "react-router-dom"
-import Cart from '../pages/Cart'
-
+import React, { useState, useEffect } from "react";
+import { Button } from "react-bootstrap";
+import "../styles/nav.css";
+import { Link } from "react-router-dom";
+import Cart from "../pages/Cart";
+import logo from "../img/logo.png";
 
 const Header = (props) => {
-    // console.log(props.cart)
-    const [show, setShow] = useState(false);
+  // console.log(props.cart)
+  const [show, setShow] = useState(false);
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
-<nav className="navbar navbar-expand-md navbar-dark bg-dark">
-    <div className="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
-        <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-            <Link to={'/'}> Home </Link>
+    //   <>
+    //   </>
+
+    <nav>
+      <div className="nav-wrapper">
+        <div>
+          <ul className="nav-left">
+            <li>
+              <Link to={"/"}> Home </Link>
             </li>
-        </ul>
-    </div>
-    <div className="mx-auto order-0">
-        <h6 className="navbar-brand mx-auto" href="#">Danny's sweet shack</h6>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
-            <span className="navbar-toggler-icon"></span>
-        </button>
-    </div>
-    <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
-        <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-                <Link to={'/allproducts'}>   All Product </Link>
+            <li >
+              <Link to={"/allproducts"}> All Product </Link>
             </li>
-            <li className="nav-item">
-                {/* <Link to={'/cart'}>  Cart </Link> */}
-                <Button variant="primary" onClick={handleShow}>
-                Cart
-                </Button>
+          </ul>
+        </div>
+        <div className="nav-mid">
+          <h6 className="nav-logo" href="#">
+            Danny's sweet shack
+          </h6>
+        </div>
+        <div className="nav-right">
+        <ul >
+            <li>
+                <a variant="primary" onClick={handleShow}> <i class="fas fa-shopping-cart fa-2x"></i></a>
                 <Cart show={show} handleShow={handleShow} handleClose={handleClose}/>
             </li>
         </ul>
     </div>
-</nav>
+      </div>
+      {/* <nav className="navbar  navbar-dark bg-dark">
+    <div >
+        <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+            <Link to={'/'}> Home </Link>
+            </li>
+            <li className="nav-item">
+                <Link to={'/allproducts'}>   All Product </Link>
+            </li>
+        </ul>
+    </div>
+    <div >
+        <h6 className="navbar-brand mx-auto" href="#">Danny's sweet shack</h6>
+    </div>
+    <div className="navbar">
+        <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+                <a variant="primary" onClick={handleShow}> <i class="fas fa-shopping-cart fa-2x"></i></a>
+                <Cart show={show} handleShow={handleShow} handleClose={handleClose}/>
+            </li>
+        </ul>
+    </div>
+</nav> */}
+    </nav>
   );
 };
 
