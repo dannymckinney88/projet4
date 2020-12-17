486418e3f178bcccbe848d6892b38e15c1ac45a3
dannymckinney88 <dannymckinney88@gmail.com>
2020-12-16 17:03:40 -0700

    removes old code

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -17,8 +17,6 @@ const CartItem = (props) => {
                 <h4 className="cart-item__details-name">{item.name}</h4>
                 <p> {item.price.formatted_with_symbol} </p>
             </div>
-            {/* <div className="col">
-            </div> */}
             <div className="col cart-btn-container">
                 <div>
                     <button type="button"  onClick={() => props.handleUpdateCartQty(item.id, item.quantity  - 1 )} title="Reduce quantity">-</button>
@@ -31,19 +29,6 @@ const CartItem = (props) => {
             </div>
         </div>
     </div>
-    //     <div className="cart-item">
-    //     <img className="cart-item__image" src={item.media.source} alt={item.name} />
-    //     <div className="cart-item__details">
-    //       <h4 className="cart-item__details-name">{item.name}</h4>
-    //       <div className="cart-item__details-qty">
-    //         <button type="button" onClick={() => props.handleUpdateCartQty(item.id, item.quantity  - 1 )} title="Reduce quantity">-</button>
-    //         <p>{item.quantity}</p>
-    //         <button type="button" onClick={() =>props.handleUpdateCartQty(item.id, item.quantity + 1 )} title="Increase quantity">+</button>
-    //       </div>
-    //       <div className="cart-item__details-price">{item.line_total.formatted_with_symbol}</div>
-    //     </div>
-    //     <button type="button">Remove</button>
-    //   </div>
     );
 }
 
